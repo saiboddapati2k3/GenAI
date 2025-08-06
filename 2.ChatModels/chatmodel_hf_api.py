38868c46372e3d0c92bc62e3b70fee5fcd22e5e6
Sai Boddapati <saiboddapatis@gmail.com>
2025-08-06 13:30:00 +0530

    implement basic rag

--- 2.ChatModels/chatmodel_hf_api.py
+++ 2.ChatModels/chatmodel_hf_api.py
@@ -1,14 +1,14 @@
-from langchain_huggingface import ChatHuggingFace , HuggingFaceEndpoint
+from langchain_openai import ChatOpenAI
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
 
-llm = HuggingFaceEndpoint(repo_id= "mistralai/Mistral-7B-Instruct-v0.2" , task="text-generation")
 
-model = ChatHuggingFace(
-    llm = llm
+model = ChatOpenAI(
+    model="gpt-4o",  
+    temperature=0.7
 )
 response = model.invoke("What is the result of 3rd test match cricket between IND vs ENG test series 2025")
 print(response.content)
